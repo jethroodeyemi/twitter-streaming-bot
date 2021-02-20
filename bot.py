868aa41eb8d1b9dbffedbf678eb5f16a7b5eb69c
semtu <69624294+semtu@users.noreply.github.com>
2021-02-20 10:18:27 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,13 +22,14 @@ def timer(days):
     media = api.media_upload(f"images/{days}.png")
     tweet = f"Days left till Buhari leaves Aso Rock: {days}"
     api.update_status(status=tweet, media_ids=[media.media_id])
+    print(days)
     return
 
 def main():
     days=829
     for i in range(1,829):
         timer(days-i)
-        time.sleep(86400)
+        time.sleep(10)
 
 if __name__ == "__main__":
     main()
