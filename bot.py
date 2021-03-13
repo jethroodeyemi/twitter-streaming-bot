f0d1a5fb6ca233d48d3bb184f0750b860107bc30
semtu <69624294+semtu@users.noreply.github.com>
2021-03-13 08:38:34 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,50 +3,42 @@ import tweepy
 import logging
 from config import create_api
 import json
-from time import sleep
+import time
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
-class FavRetweetListener(tweepy.StreamListener):
-    def __init__(self, api):
-        self.api = api
-        self.me = api.me()
+def check_mentions(api, keywords, since_id):
+    logger.info("Retrieving mentions")
+    new_since_id = since_id
+    for tweet in tweepy.Cursor(api.search,keywords,
+        since_id=since_id).items():
+        new_since_id = max(tweet.id, new_since_id)
+        if tweet.in_reply_to_status_id is not None:
+            continue
+        if any(keyword in tweet.text.lower() for keyword in keywords):
+            logger.info(f"Answering to {tweet.user.name}")
 
-    def on_status(self, tweet):
-        logger.info(f"Processing tweet id {tweet.id}")
-        if tweet.in_reply_to_status_id is not None or \
-            tweet.user.id == self.me.id:
-            # This tweet is a reply or I'm its author so, ignore it
-            return
-        if not tweet.favorited:
-            # Mark it as Liked, since we have not done it yet
-            try:
-                tweet.favorite()
-            except Exception as e:
-                logger.error("Error on fav", exc_info=True)
-        if not tweet.retweeted:
-            # Retweet, since we have not retweeted it yet
-            try:
-                tweet.retweet()
-            except Exception as e:
-                logger.error("Error on fav and retweet", exc_info=True)
-        # if not tweet.user.following:
-        #     # Follow
-        #     try:
-        #         tweet.user.follow()
-        #     except Exception as e:
-        #         logger.error("Error on fav, retweet and follow", exc_info=True)
-        sleep(50)
+            if not tweet.user.following:
+                tweet.user.follow()
 
-    def on_error(self, status):
-        logger.error(status)
+            api.update_status(
+                status=f"Thanks for mentioning Wizkid in your tweet, this is an automatic reply. Follow the Wizkid FC Bot account for everything concerning Wizkid. IFB automatically. \n({datetime.today()})",
+                in_reply_to_status_id=tweet.id,
+                auto_populate_reply_metadata=True
+            )
+            time.sleep(300)
+            # api.update_with_media('/image.jpg', status='message')
+    return new_since_id
 
-def main(keywords):
+def main():
     api = create_api()
-    tweets_listener = FavRetweetListener(api)
-    stream = tweepy.Stream(api.auth, tweets_listener)
-    stream.filter(track=keywords, languages=["en"])
+    since_id = 1
+    while True:
+        since_id = check_mentions(api, ["wizkid"], since_id)
+        logger.info("Waiting...")
+        time.sleep(10)
 
 if __name__ == "__main__":
-    main(["Wizkid"])
+    main()
