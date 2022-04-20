1bff8e8ce017d856c3522ec8c77a9e93b68324c4
semtu <semtu001@gmail.com>
2022-04-20 21:12:34 +0200

    first

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-jeth
+# Twitter streaming bot
+
+Continuously streams twitter for keyword **Wizkid** then replies the target tweet with the the message:
+> Thanks for mentioning Wizkid in your tweet, this is an automatic reply. Follow the Wizkid FC Bot account for everything concerning Wizkid. IFB automatically
--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import tweepy
 logger = logging.getLogger()
 
 def create_api():
-    consumer_key = "AidDo5F5GIAV29AA6JllMMown"
-    consumer_secret = "ssTbjY4WZSQgIPo0W5Iwwghng40MQqvfHMn3DPaxAxheqGgpKf"
-    access_token = "1243164206481973249-z6SrtkjzjDHixIviYJdfV48rTlKBBB"
-    access_token_secret = "5rp3DWr8TMRtHIepAfmrXKJ72QjMNW2WtDQ215ntdVOAO"
+    consumer_key = "8XHm6fVwUMco0oYE19fGMmVWL"
+    consumer_secret = "zW5hYwC7tCM2sKFxCqZ98ula8cFKxiODuzFg6qwohOsPhjBpP2"
+    access_token = "1243164206481973249-k2p5hsyLMb7NLK6UfHPSkDGm9FWNiu"
+    access_token_secret = "SVDdEbqESmwWoLgCB9rec8BLpYMTZMymdhqhUAvpAucx4"
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
